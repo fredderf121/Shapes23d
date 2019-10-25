d2a084d21c7d0f23a611b328e060b9a91ecd1024
S332896109 <S332896109@LDHS-BYT-122538.ad.ocdsb.ca>
2019-10-25 12:25:35 -0400

    Merge origin/3D into 3D
    Conflicts:
    	Shapes23d/src/Shapes23d/Prism.java

--- Shapes23d/src/Shapes23d/Pyramid.java
+++ Shapes23d/src/Shapes23d/Pyramid.java
@@ -13,7 +13,9 @@ public class Pyramid extends Shape3D implements threeDimensional {
     public void create(){
         initSides("base");
         this.height  = getValue("Please enter in the height of the pyramid");
+        
         this.surface_area = calculateSurfaceArea(this.height, this.base_area, this.apothem, this.sideN, this.lengths.get(0));
+        
         this.volume = calculateVolume(this.height, this.base_area);
         displayResults();
     }
@@ -30,10 +32,12 @@ public class Pyramid extends Shape3D implements threeDimensional {
     @Override
     public double calculateSurfaceArea(double height, double base_area, 
                         double apothem, int num_sides, double side_length){
-        
+        System.out.println(apothem + "     "+ base_area);
         double slant_length =  Math.sqrt(Math.pow(height, 2)+Math.pow(apothem, 2));
+        System.out.println(slant_length);
         return base_area + slant_length*side_length/2*num_sides;
         
     }
+    
     
 }
