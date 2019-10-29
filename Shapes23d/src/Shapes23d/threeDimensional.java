fe7f3ede089abd9b03f6ef77d100fde2049c9545
S332896109 <S332896109@LDHS-BYT-122538.ad.ocdsb.ca>
2019-10-29 17:27:29 -0400

    Merge origin/3D into 3D
    Conflicts:
    	Shapes23d/src/Shapes23d/Shape.java
    	Shapes23d/src/Shapes23d/Shape3D.java

--- Shapes23d/src/Shapes23d/threeDimensional.java
+++ Shapes23d/src/Shapes23d/threeDimensional.java
@@ -1,7 +1,5 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Interface that connects the Sphere, Pyramid, and Prism classes
  */
 package Shapes23d;
 
@@ -11,20 +9,21 @@ package Shapes23d;
  */
 public interface threeDimensional {
     
-    default public double calculateVolume(double height, double base_area){
+    default public double calculateVolume(double height, double base_area){//Method for the prism and pyramid
         return 0;
     }
-    default public double calculateVolume(double radius){
+    default public double calculateVolume(double radius){//Method for the sphere
         return 0;
     }
     
-    default public double calculateSurfaceArea(double height, double base_area, double perimeter){
+    default public double calculateSurfaceArea(double height, double base_area, double perimeter){//Method for the prism
         return 0;
     }
     default public double calculateSurfaceArea(double height, double base_area, double apothem, int num_sides, double side_length){
+        //Method for the pyramid
         return 0;
     }
-    default public double calculateSurfaceArea( double radius){
+    default public double calculateSurfaceArea( double radius){//Method for the sphere
         return 0;
     }
 }
